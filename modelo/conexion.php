f4a5f15743538523c7b0072c6e361a56fe501a18
Eduardo <datastorage167@gmail.com>
2020-11-20 10:08:30 -0600

    Modificamos la estructura de la conexion colocando un try-catch

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,6 +1,13 @@
 <?php
-$conexion=mysqli_connect("fdb29.awardspace.net","3622290_pruebas","admin369");
- mysqli_select_db($conexion,"3622290_pruebas")OR DIE ("Error:No es posible establecer la conexion");
+try{
+    $conexion=mysqli_connect("fdb29.awardspace.net","3622290_pruebas","admin369");
+    mysqli_select_db($conexion,"3622290_pruebas");
+    echo" La conexiones es correcta ------> :)";
 
+}
+catch (Exception $e)
+{
+    die("El error de Conexión es: ". $e->getMessage());
+}
 ?> 
 
