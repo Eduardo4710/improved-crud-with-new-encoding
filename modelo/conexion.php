027411c6d842a0be5829a5c0fd7c3d2bb1c92739
Eduardo <datastorage167@gmail.com>
2020-11-20 09:26:36 -0600

    Disenio de mi tabla : D1

---
+++ modelo/conexion.php
@@ -0,0 +1,6 @@
+<?php
+$conexion=mysqli_connect("fdb29.awardspace.net","3622290_pruebas","admin369");
+ mysqli_select_db($conexion,"3622290_pruebas")OR DIE ("Error:No es posible establecer la conexion");
+
+?> 
+
